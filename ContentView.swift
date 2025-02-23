168ad2246016c6b9be69150d0ef8cec058514d6b
spark-o-petit <petitspark247@gmail.com>
2025-02-24 05:09:51 +0900

    feat: Delete unnecessary items

--- ContentView.swift
+++ ContentView.swift
@@ -2,20 +2,25 @@ import SwiftUI
 
 struct ContentView: View {
   @StateObject private var glucoseData = GlucoseData()
-
-    var body: some View {
-        TabView {
-            HomeView(glucoseData: glucoseData)
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Home")
-                }
-            
-            HistoryView(glucoseData: glucoseData)
-                .tabItem {
-                    Image(systemName: "calendar")
-                    Text("History")
-                }
+  
+  var body: some View {
+    TabView {
+      HomeView(glucoseData: glucoseData)
+        .tabItem {
+          Image(systemName: "house.fill")
+          Text("Home")
+        }
+      
+      HistoryView(glucoseData: glucoseData)
+        .tabItem {
+          Image(systemName: "calendar")
+          Text("History")
+        }
+      
+      ReportView(glucoseData: glucoseData)
+        .tabItem {
+          Label("Report", systemImage: "chart.bar.fill")
         }
     }
+  }
 }
---
+++ Untitled 2.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled 2.swift
+//  SimpleGlucoseTracker
+//
+//  Created by alex on 2/24/25.
+//
+
